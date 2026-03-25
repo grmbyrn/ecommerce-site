f543965a66b8344f0b970b306fee8f75ecffa237
Graeme <grmbrn89@gmail.com>
2026-03-25 13:57:40 +0100

    add responsive menu sheet to nav and add sample products and Productlist

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,10 @@
-import { Button } from "@/components/ui/button";
+import sampleData from "@/db/sample-data";
+import ProductList from "@/components/shared/product/product-list";
 
 export default function Home() {
   return (
-    <Button>Get Started</Button>
+    <>
+      <ProductList data ={sampleData.products} title="Newest Arrivals" limit={4} />
+    </>
   );
 }
